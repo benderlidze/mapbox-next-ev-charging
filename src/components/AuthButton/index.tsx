de4d94367b2d7e1b22a26f061c00f98dcf60bb7b
Serg <benderlio@gmail.com>
2023-10-10 21:20:48 +0200

    supabase auth

--- src/components/AuthButton/index.tsx
+++ src/components/AuthButton/index.tsx
@@ -1,59 +1,97 @@
 "use client";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
+import {
+  Session,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
 import { signIn, signOut } from "next-auth/react";
-import { Session } from "next-auth";
+import { useEffect, useState } from "react";
 
 export const AuthButton = () => {
-  const { data: session } = useSession();
-
   const [isOpen, setIsOpen] = useState(false);
   const handleButtonCLick = () => {
     console.log("clicked");
     setIsOpen(!isOpen);
   };
 
-  console.log("session", session);
+  // const UserButton = () => {
+  //   if (session && session.user?.name) {
+  //     return (
+  //       <div className="flex flex-row">
+  //         {session.user?.image && (
+  //           <img
+  //             src={session.user?.image}
+  //             className="w-5 rounded-full mr-2"
+  //             alt="user image"
+  //           />
+  //         )}
+  //         <strong>{session.user.name}</strong>
+  //       </div>
+  //     );
+  //   }
+  // };
 
-  const UserButton = () => {
-    if (!session) {
-      return "Guest";
-    }
+  const [session, setUser] = useState<Session>();
+  const supabase = createClientComponentClient();
 
-    if (session && session.user?.name) {
-      return (
-        <div className="flex flex-row">
-          {session.user?.image && (
-            <img
-              src={session.user?.image}
-              className="w-5 rounded-full mr-2"
-              alt="user image"
-            />
-          )}
-          <strong>{session.user.name}</strong>
-        </div>
-      );
-    }
+  const handleLogInGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
   };
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    window.location.href = "/";
+  };
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      session && setUser(session);
+    });
+  }, [supabase]);
+
+  useEffect(() => {
+    async function getProfile() {
+      console.log("session", session);
+      let { data, error } = await supabase
+        .from("profiles")
+        .select(`username, website, avatar_url`)
+        .eq("id", "fe5285a1-8b35-4861-bea4-91943cb33ee1")
+        .single();
+
+      console.log("data", data);
+    }
+
+    getProfile();
+  }, [session]);
 
   return (
     <>
       <div
         onClick={handleButtonCLick}
-        className="p-2 border rounded-md  cursor-pointer hover:bg-gray-100 select-none"
+        className="p-2 border rounded-md cursor-pointer hover:bg-gray-100 select-none"
       >
-        <UserButton />
+        {session && <div className="flex">{session && session.user.email}</div>}
+        {!session && <div className="flex">Sign in</div>}
       </div>
       {isOpen && (
         <div className="absolute z-10 top-20 right-2 w-48 bg-white border rounded-xl shadow-md p-4">
           {!session && (
-            <button style={{ marginRight: 10 }} onClick={() => signIn()}>
-              Sign in
-            </button>
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center">
+                <button onClick={handleLogInGoogle}>Sign in with Google</button>
+              </div>
+              <div className="flex items-center">
+                <button onClick={handleLogInGoogle}>Sign in with Apple</button>
+              </div>
+              <div className="flex items-center">
+                <button onClick={handleLogInGoogle}>Sign in with FB</button>
+              </div>
+            </div>
           )}
 
           {session && (
-            <button style={{ marginRight: 10 }} onClick={() => signOut()}>
+            <button style={{ marginRight: 10 }} onClick={() => handleSignOut()}>
               Sign Out
             </button>
           )}
