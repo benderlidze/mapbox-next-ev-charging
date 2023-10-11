4d00fef22bee8ef01154dd3ad66c95e4af2c58e4
Serg <benderlio@gmail.com>
2023-10-11 18:31:36 +0200

    vercel

--- pages/api/load-pins/index.ts
+++ pages/api/load-pins/index.ts
@@ -1,14 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-  console.log("req", req.body);
-
   // const apiKey = "Pf2pX3LeQgclbSAbkvRp927FRkYnVqMZEeYglPS4";
   const apiKey = "DEMO_KEY";
   const query = req.body.join("&");
-
   const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=200&fuel_type=ELEC&${query}`;
-
   console.log("url", url);
 
   const data = await fetch(url, {
