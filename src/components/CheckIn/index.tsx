2a7d5abd2e2cd0256c4054767336cea7e4cd7ae9
Serg <benderlio@gmail.com>
2023-10-16 13:47:54 +0200

    form validation

--- src/components/CheckIn/index.tsx
+++ src/components/CheckIn/index.tsx
@@ -3,6 +3,9 @@ import { ChargerType, ChargerTypes } from "@components/ChargerTypes";
 import { PinProps } from "@components/PinPopup";
 import StarRating from "@components/StarRating";
 import { ToggleSwitcher } from "@components/ToggleSwitcher";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface CheckInProps {
   pin: PinProps;
@@ -10,107 +13,142 @@ interface CheckInProps {
 }
 
 export const CheckIn = ({ pin, vehicles }: CheckInProps) => {
+  const supabase = createClientComponentClient();
+  const { register, handleSubmit } = useForm();
+
+  const handleFormSubmit = () => {
+    handleSubmit((data) => {
+      console.log("data===>>>>", data);
+
+      // const { error } = await supabase
+      //   .from("checkins")
+      //   .insert({ comments: comment });
+    });
+  };
+
   return (
-    <div
-      className="flex flex-col gap-2 overflow-auto max-h-[300px] 
-    tall:overflow-visible tall:max-h-screen "
+    <form
+      onSubmit={handleSubmit((data) => {
+        console.log("data===>>>>", data);
+
+        // const { error } = await supabase
+        //   .from("checkins")
+        //   .insert({ comments: comment });
+      })}
     >
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-xl">Vehicle</div>
-        <div className="text-sm">
-          <select
-            name=""
-            id=""
-            className="w-32 p-2 bg-gray-300 rounded-2xl border-none"
-          >
-            <option value="">Select</option>
-            {vehicles.map((vehicle) => (
-              <option key={vehicle.id} value={vehicle.id}>
-                {vehicle.vehicle}
-              </option>
-            ))}
-          </select>
+      <div
+        className="flex flex-col gap-2 overflow-auto max-h-[300px] 
+    tall:overflow-visible tall:max-h-screen "
+      >
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-xl">Vehicle Type</div>
+          <div className="text-sm">
+            <select
+              {...register("vehicle_id")}
+              className="w-32 p-2 bg-gray-300 rounded-2xl border-none"
+            >
+              <option value="">Select</option>
+              {vehicles.map((vehicle) => (
+                <option key={vehicle.id} value={vehicle.id}>
+                  {vehicle.vehicle}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-xl">Overall rating</div>
-        <div className="text-sm">
-          <StarRating rating={3} />
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-xl">Overall rating</div>
+          <div className="text-sm">
+            <StarRating rating={3} />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-sm">Charging speed</div>
-        <div className="text-sm">
-          <StarRating rating={4} />
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-sm">Charging speed</div>
+          <div className="text-sm">
+            <StarRating rating={4} />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-sm">Reliability</div>
-        <div className="text-sm">
-          <StarRating rating={3} />
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-sm">Reliability</div>
+          <div className="text-sm">
+            <StarRating rating={3} />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-sm">Safety & Cleanliness</div>
-        <div className="text-sm">
-          <StarRating rating={3} />
+
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-sm">What was your maximum charge rate (kW)?</div>
+          <div className="text-sm">
+            <input
+              type="text"
+              className="w-16  p-2 bg-gray-300 rounded-2xl border-none"
+              placeholder="KWs"
+              {...register("energy_consumed")}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-xl">Power consumed</div>
-        <div className="text-sm">
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-sm">How many chargers were working?</div>
+          <div className="text-sm">
+            <input
+              type="text"
+              className="w-16  p-2 bg-gray-300 rounded-2xl border-none"
+              placeholder="num"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-sm">Charging time</div>
+          <div className="text-sm">
+            <input
+              type="text"
+              className="w-16  p-2 bg-gray-300 rounded-2xl border-none"
+              placeholder="mins"
+              {...register("charging_session_duration")}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col ">
+          <div className="text-xl">Plug type</div>
+          <div className="flex flex-row">
+            <ChargerTypes types={pin.ev_connector_types as ChargerType[]} />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-sm">Were you able to charge your vehicle?</div>
+          <div className="text-sm">
+            <ToggleSwitcher />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-sm">
+            Did you wait in line ( 5 mins) to charge?
+          </div>
+          <div className="text-sm">
+            <ToggleSwitcher />
+          </div>
+        </div>
+        <div className="flex flex-col ">
+          <div className="text-xl">Comments </div>
+          <div className="">
+            <textarea
+              {...register("comments")}
+              className="p-2 w-full min-h-[100px] bg-gray-300 rounded-2xl "
+            ></textarea>
+          </div>
+        </div>
+        <div className="flex justify-center gap-1">
+          <div className="">
+            <input type="checkbox" {...register("anonymous_flag")} />
+          </div>
+          <div className="text-xs">I wish to remain anonymous</div>
+        </div>
+        <div className="flex justify-center gap-1">
           <input
-            type="text"
-            className="w-16  p-2 bg-gray-300 rounded-2xl border-none"
-            placeholder="KWs"
+            type="submit"
+            className="select-none w-1/2 flex justify-center text-white rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 cursor-pointer p-2 width-fit mt-2 mb-2"
           />
         </div>
       </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-xl">Charging time</div>
-        <div className="text-sm">
-          <input
-            type="text"
-            className="w-16  p-2 bg-gray-300 rounded-2xl border-none"
-            placeholder="mins"
-          />
-        </div>
-      </div>
-      <div className="flex flex-col ">
-        <div className="text-xl">Plug type</div>
-        <div className="flex flex-row">
-          <ChargerTypes types={pin.ev_connector_types as ChargerType[]} />
-        </div>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="text-xl">Wait to charge? </div>
-        <div className="text-sm">
-          <ToggleSwitcher />
-        </div>
-      </div>
-      <div className="flex flex-col ">
-        <div className="text-xl">Comments </div>
-        <div className="">
-          <textarea
-            name=""
-            className="p-2 w-full min-h-[100px] bg-gray-300 rounded-2xl "
-          ></textarea>
-        </div>
-      </div>
-      <div className="flex justify-center gap-1">
-        <div className="">
-          <input type="checkbox" />
-        </div>
-        <div className="text-xs">I wish to remain anonymous</div>
-      </div>
-      <div className="flex justify-center gap-1">
-        <div
-          onClick={() => {}}
-          className="select-none w-1/2 flex justify-center text-white rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 cursor-pointer p-2 width-fit mt-2 mb-2"
-        >
-          Submit
-        </div>
-      </div>
-    </div>
+    </form>
   );
 };
